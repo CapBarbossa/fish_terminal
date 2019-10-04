f8639c248792ff31db5c73f022e569707fcce4ae
CapBarbossa <47692079+CapBarbossa@users.noreply.github.com>
2019-10-04 23:20:47 +0800

    Update run_ubuntu.sh

--- run_ubuntu.sh
+++ run_ubuntu.sh
@@ -2,7 +2,7 @@ apt-add-repository ppa:fish-shell/release-3
 apt-get update
 apt-get install fish
 chsh -s /usr/bin/fish
-cd ~/.config
+mkdir -p ~/.config && cd ~/.config
 git clone https://github.com/CapBarbossa/fish_terminal.git
 rm -rf fish
 mv fish_terminal fish
